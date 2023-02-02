305e0fe8eecf49675433e45a781972d6e1db4801
carol695 <63822072+carol695@users.noreply.github.com>
2023-02-02 18:29:48 -0500

    Terminando cache

--- src/main/java/edu/escuelaing/arem/app/cache.java
+++ src/main/java/edu/escuelaing/arem/app/cache.java
@@ -4,6 +4,10 @@ package edu.escuelaing.arem.app;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Memoria cache de la API
+ */
+
 public class cache {
 
     private static cache instance;
@@ -14,11 +18,25 @@ public class cache {
      */
     private cache() {
     }
+    
+    /**
+     * Método que almacena una consulta a la API en caso
+     * de que no se encuentre almacenado en el HashMap,
+     * en caso dado que se encuentre consulta sobre este
+     * @param value url de la API de la cual se va a consultar la información de las películas
+     * @param title Nombre de la película buscada
+     */
 
     public void saveQuery(String value, String title){
         cache.put(title, value);
     }
-
+    
+    /**
+     * Metodo que obtiene la instancia
+     *
+     * @param key Key a buscar
+     * @return Valor asociado a la key
+     */
 
     public static cache getInstance() {
         if(instance == null){
